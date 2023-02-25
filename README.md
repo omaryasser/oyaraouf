43b8f526248fbab0e944b68fd5fbc90f9b20af8c
Omar Yasser <oyaraouf@gmail.com>
2023-02-25 14:12:37 +0000

    Bootstrap a personal website

--- README.md
+++ README.md
@@ -65,6 +65,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/a
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
 
+docker build -t oyaraouf . && docker tag oyaraouf europe-west8-docker.pkg.dev/oyaraouf/oyaraouf/oyaraouf & docker push europe-west8-docker.pkg.dev/oyaraouf/oyaraouf/oyaraouf:latest && gcloud run services replace service.yaml
+
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import avatar from './avatar.png';
 import './App.css';
-
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <link rel="canonical" href="http://" />
+
+        <p> WIP </p>
+        <img src={avatar} className="App-logo" alt="avatar" />
+        <table>
+          <tr>
+          <td><SocialIcon url="https://www.linkedin.com/in/omar-morsi/" /></td>
+          <td> <SocialIcon url="https://twitter.com/oyaraouf" /></td>
+          {/* <td> <SocialIcon url="https://ask.fm/oyaraouf" /></td>
+          <td> <SocialIcon url="https://curiouscat.live/oyaraouf" /></td> */}
+          <td> <SocialIcon url="https://www.facebook.com/omar.yasser.morsi" /></td>
+          </tr>
+        </table>
       </header>
     </div>
   );
