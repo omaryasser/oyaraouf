6b71d2199c7c191d231f3882f26f74a4180054aa
Omar Yasser <oyaraouf@gmail.com>
2025-01-02 19:11:40 +0000

    Add the CNAME file automatically after build

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
-  // basePath: '/oyaraouf',
-  // assetPrefix: '/oyaraouf',
   images: {
     unoptimized: true
   },
