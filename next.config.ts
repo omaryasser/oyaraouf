7e84b1cbe78b36516d4798d90c5af6fb9fb37185
Omar Yasser <oyaraouf@gmail.com>
2025-01-02 17:27:16 +0000

    Add path prefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',
+  basePath: '/oyaraouf',
+  assetPrefix: '/oyaraouf',
   images: {
     unoptimized: true
   },
